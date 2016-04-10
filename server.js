ecc44b6811a27bb1906f5a92bf0836ed78f973be
Kevin Cohen <kevin.cohen26@gmail.com>
2016-04-10 01:33:18 -0400

    Modules, database integration

--- server.js
+++ server.js
@@ -1,5 +1,53 @@
 var app = require("express")();
+var mongoose = require('mongoose');
+
+var bodyParser = require('body-parser');
+
+
 var http = require('http').Server(app);
+
+// var cors = require('cors');
+
+// app.use(cors());
+ 
+// app.use(cors({
+// 	allowedOrigins: [
+// 		'github.com'
+// 	]
+// }))
+
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+
+mongoose.connect('mongodb://localhost/Hackpsu2016');
+
+
+// DATABASE INTEGRATION
+
+var Schema = new mongoose.Schema({
+  name:       		 String,     //username
+  email:  			 String,
+  stackOverflowData: JSON
+});
+
+var user = mongoose.model('Users',Schema);
+//var jsonObject; 
+
+app.post('/hackpsu/insert',function(req, res) {
+
+  console.log(req.body.stackoverflow);
+
+  // new user({
+  // name:  "Kevin",
+  // email:       "kevinco@gmail.com",
+  // stackOverflowData:req.body
+  // }).save(function(err,doc){
+  //   if(err)res.json(err);
+  // });
+
+}); 
 
 
 app.get('/',function(req,res){
