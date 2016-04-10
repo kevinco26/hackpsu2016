5d83f3fcf48c629ca6d8972e4f2f6e383fe39313
Scott Miller <sam6258@psu.edu>
2016-04-09 20:44:45 -0400

    First push

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.get('/',function(req,res){
    res.sendFile(__dirname + '/homepage.html');
 });
 
+app.get('/homepage.css',function(req,res){
+   res.sendFile(__dirname + '/homepage.css');
+});
+
+app.get('/assets/bg.png',function(req,res){
+   res.sendFile(__dirname + '/assets/bg.png');
+});
+
+app.get('/homepage.js',function(req,res){
+   res.sendFile(__dirname + '/homepage.js');
+});
 
 http.listen(8080, function() {
   console.log('Server running at http://127.0.0.1:8080');
